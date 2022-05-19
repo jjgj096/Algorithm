2ffbd9e55946e3913d9a8c2aae9fffadbc02aaf5
JUHO YUN <81611401+jjgj096@users.noreply.github.com>
2022-05-19 19:28:05 +0900

    Update recursive_search.c

--- recursive_search.c
+++ recursive_search.c
@@ -2,58 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int recur_search(int low, int high, int* list, int target) {		//����Լ��� ����� ����Ž�� �Լ� ����
+int recur_search(int low, int high, int* list, int target) {		
 	int mid;
 
-	if (low > high)		//target�� �迭�� �������� �ʴ� ���
+	if (low > high)		
 		return -1;
 
-	mid = (low + high) / 2;		//�߰��������� Ž�� ����.
+	mid = (low + high) / 2;		
 
-	if (target == list[mid])	//target�� ã�� ���
-		return mid;				//�ش� mid�� ��ȯ
+	if (target == list[mid])	
+		return mid;				
 
-	else if (target < list[mid])	//target�� �߰������� ���� ���
-		return recur_search(low, mid - 1, list, target);	//�� ���ʿ��� ã�� ���� high�� ����
+	else if (target < list[mid])	
+		return recur_search(low, mid - 1, list, target);	
 
-	else {				//target�� �߰������� Ŭ ���
-		return recur_search(mid + 1, high, list, target);	//�� �����ʿ��� ã�� ���� low�� ����
+	else {				
+		return recur_search(mid + 1, high, list, target);	
 	}
 }
 
 int main(void) {
-	int n;			//�迭�� ũ�⸦ ������ ����.
+	int n;			
 	int i;
 	int* ary;
 
 
 	printf("배열 크기를 입력하세요 :");
-	scanf("%d", &n);	//�迭�� ũ�⸦ �Է¹ް�, n�� ����.
+	scanf("%d", &n);	
 
-	ary = (int*)malloc(sizeof(int) * n);	//�Է¹��� n���� ũ���� �迭�� ������.
+	ary = (int*)malloc(sizeof(int) * n);	
 
 	printf("배열에 들어갈 수를 입력하세요: ");
-	for (i = 0; i < n; i++) {				//n���� ���� ������� �迭�� ������.
+	for (i = 0; i < n; i++) {				
 		scanf("%d", &ary[i]);
 	}
 
-	int key;								//ã�� ��ǥ�� ����
+	int key;								
 
-	printf("찾을 수를 입력하세요 : ");		//ã�� Ÿ���� �Է¹���.
+	printf("찾을 수를 입력하세요 : ");		
 	scanf("%d", &key);
 
 	printf("이진 탐색을 시작합니다.");
 
-	int index;								//recur_search �Լ��� ������ ���� int ����
-	index = recur_search(0, n, ary, key);	//����Լ� ȣ��
+	int index;								
+	index = recur_search(0, n, ary, key);	
 
-	if (index == -1)						//���ϰ��� ���� ��Ȳ�� ����.
+	if (index == -1)						
 		printf("실패");
 	else
 		printf("%d", index+1);
 
 
-	free(ary);			//�����Ҵ��� ������.
+	free(ary);			
 
 	return 0;
 
